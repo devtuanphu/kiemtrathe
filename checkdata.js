8e44f4436a16d05e93ec6579197d2a7835a663fd
devtuanamit1409 <nguyentuanamit@gmail.com>
2024-12-14 19:21:22 +0700

    first commit

---
+++ checkdata.js
@@ -0,0 +1,14 @@
+
+    const loaithe= document.formdata.loaithe.value
+    const soseri = document.formdata.soseri.value
+    const mathe = document.formdata.mathe.value
+    const sđt = document.formdata.sđt.value
+    const btnkiemtra= document.getElementById('btn-kiemtra')
+    const change = document.getElementById('changeText')
+
+    
+
+
+
+
+
